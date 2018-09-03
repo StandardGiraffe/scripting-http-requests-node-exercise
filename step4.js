2017c579ad97c8993cbc553da15bb677ab0c0c16
StandardGiraffe <dannyfekete@gmail.com>
2018-09-03 18:08:05 +0000

    Modified step3.js to actually pass the argument-collecting variable into the function (rather than call the variable independently within it).  Built step4.js to take a callback which in this case is print functionality.

--- step4.js
+++ step4.js
@@ -1,9 +1,7 @@
 const https = require("https");
 
+function getHTML (options, callback) {
 
-function getAndPrintHTML (options) {
-
-// Takes parameters from the command line: (host, path).
 
   https.get(options, (response) => {
     let dataBuffer = "";
@@ -11,22 +9,26 @@ function getAndPrintHTML (options) {
 
     response.on("data", (data) => {
       dataBuffer += data;
-      console.log("Received a data chunk and added it to the buffer.");
+      console.log("Received a data chunk and added it to the buffer.")
     });
 
     response.on("end", () => {
-      console.log("Data acquired.  Printing buffer:\n")
-      console.log(dataBuffer);
+      console.log("Download complete.  Returning buffer.");
+      callback(dataBuffer);
     });
 
   });
-
 
 }
 
 const requestOptions = {
   host: process.argv[2],
   path: process.argv[3]
+};
+
+function printHTML (html) {
+  console.log(html);
 }
 
-getAndPrintHTML(requestOptions);
+
+getHTML(requestOptions, printHTML);
